912f5206ddba2afc40302fb85352d351a13dcb10
KalebYigezu <43226925+KalebYigezu@users.noreply.github.com>
2024-10-19 14:28:11 -0700

    Update Cryptography.py

--- Cryptography.py
+++ Cryptography.py
@@ -1,1 +1,23 @@
+from cryptography.fernet import Fernet
 
+
+
+
+key = Fernet.generate_key()
+
+print(key)
+
+msg = "Welcome to Channel".encode()
+
+f_obj = Fernet(key)
+
+encrypted_msg = f_obj.encrypt(msg)
+
+print(encrypted_msg)
+
+
+
+
+decrypted_msg = f_obj.decrypt(encrypted_msg)
+
+print(decrypted_msg)
